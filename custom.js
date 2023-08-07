84bf29ed3321aa4070cf27c11375f1ce5fa25f49
GreatValour <oluwanifemivalour@outlook.com>
2023-08-07 06:23:21 -0400

    Test 4

--- custom.js
+++ custom.js
@@ -1,98 +1,61 @@
-// Function to handle form submission
-function handleFormSubmission(event) {
-    event.preventDefault();
-  
-    // Validate form inputs before proceeding
-    if (validateForm()) {
+< !--Your HTML form code here-- >
+
+    <script>
+  // Function to handle form submission
+        function handleFormSubmission(event) {
+            event.preventDefault();
+
+        // Validate form inputs before proceeding
+        if (validateForm()) {
       // Get form data
       const formData = new FormData(event.target);
-      const userData = {};
+        const userData = { };
       formData.forEach((value, key) => {
-        userData[key] = value;
+            userData[key] = value;
       });
-  
-      // Store user data in localStorage
-      localStorage.setItem('user', JSON.stringify(userData));
-  
-      // Show success message
-      Swal.fire({
-        icon: 'success',
-        title: 'Registration Successful!',
-        text: 'Your account has been created successfully!',
-        confirmButtonText: 'OK',
-      }).then(() => {
-        // Redirect to the dashboard page
-        window.location.href = 'login.php.html';
-      });
+
+        // Make a POST request to data.php to store the user data
+        fetch('data.php', {
+            method: 'POST',
+        body: JSON.stringify(userData),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.success) {
+            // Show success message
+            Swal.fire({
+                icon: 'success',
+                title: 'Registration Successful!',
+                text: 'Your account has been created successfully!',
+                confirmButtonText: 'OK',
+            }).then(() => {
+                // Redirect to the login page
+                window.location.href = 'login.php.html';
+            });
+          } else {
+            // Show error message
+            Swal.fire({
+                icon: 'error',
+                title: 'Registration Failed',
+                text: 'An error occurred while processing your registration. Please try again later.',
+                confirmButtonText: 'OK',
+            });
+          }
+        })
+        .catch((error) => {
+            // Show error message for any network or server-side issues
+            Swal.fire({
+                icon: 'error',
+                title: 'Registration Failed',
+                text: 'An error occurred while processing your registration. Please try again later.',
+                confirmButtonText: 'OK',
+            });
+        });
     }
   }
-  
-  // Function to validate form inputs
-  function validateForm() {
-    const username = document.getElementById('username').value;
-    const fullname = document.getElementById('username2').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const Cpassword = document.getElementById('Cpassword').value;
-    const termsCheckbox = document.getElementById('terms');
-  
-    let isValid = true;
-  
-    // Validation for username
-    if (!username.trim()) {
-      isValid = false;
-      document.getElementById('username-error').textContent = 'Username is required';
-    } else {
-      document.getElementById('username-error').textContent = '';
-    }
-  
-    // Validation for fullname
-    if (!fullname.trim()) {
-      isValid = false;
-      document.getElementById('username-error2').textContent = 'Fullname is required';
-    } else {
-      document.getElementById('username-error2').textContent = '';
-    }
-  
-    // Validation for email
-    if (!email.trim()) {
-      isValid = false;
-      document.getElementById('email-error').textContent = 'Email is required';
-    } else {
-      document.getElementById('email-error').textContent = '';
-    }
-  
-    // Validation for password
-    if (!password.trim()) {
-      isValid = false;
-      document.getElementById('pwd-error').textContent = 'Password is required';
-    } else {
-      document.getElementById('pwd-error').textContent = '';
-    }
-  
-    // Validation for confirm password
-    if (!Cpassword.trim()) {
-      isValid = false;
-      document.getElementById('pwd-error2').textContent = 'Confirm Password is required';
-    } else if (password !== Cpassword) {
-      isValid = false;
-      document.getElementById('pwd-error2').textContent = 'Passwords do not match';
-    } else {
-      document.getElementById('pwd-error2').textContent = '';
-    }
-  
-    // Validation for terms checkbox
-    if (!termsCheckbox.checked) {
-      isValid = false;
-      document.getElementById('terms-feedback').textContent = 'You must agree to the terms of service';
-    } else {
-      document.getElementById('terms-feedback').textContent = '';
-    }
-  
-    return isValid;
-  }
-  
-  // Add event listener to the form submit button
-  const registerForm = document.getElementById('signup_form');
-  registerForm.addEventListener('submit', handleFormSubmission);
-  
+
+    // Rest of your validateForm function and event listener code remains the same
+    </script>
